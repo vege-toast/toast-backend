a2a5e8519c99e2bac0ac4f41f44b9a843c45aee2
Yoonoh <xkakak142@naver.com>
2022-03-19 18:12:47 +0900

    fix: add credentials option at response

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,6 +4,8 @@ const passport = require('../config/passport.js');
 
 router.get('/', (req, res) => {//로그인 상태 확인
   res.header("Access-Control-Allow-Origin", "http://localhost:8000");
+  res.header("Access-Control-Allow-Credentials", "true");
+
     if(req.user){
         console.log(req.user);
         res.status(200);
@@ -40,6 +42,7 @@ router.get('/google/callback', passport.authenticate('google', {
   console.log(req.user);
   console.log("로그인 완료");
   res.header("Access-Control-Allow-Origin", "http://localhost:8000");
+  res.header("Access-Control-Allow-Credentials", "true");
   res.redirect("http://localhost:8000");
 });
 
